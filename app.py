240dbe61b53dbed40500188d7a4e73a7cadce30d
ArthurDav <lachat.arthur74@gmail.com>
2019-11-28 16:00:48 +0900

    update of the comments

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ app = Flask(__name__)
 
 """ Define the route of the application, route are defined URLS, route can bedefined with a type int str etc  """
 
-@app.route('/user/<name>')
-def user(name):
-    return  '<h1>Hello, %s!<h1>' % name
+@app.route('/')
+def index():
+ return '<h1>Welcom!</h1>'
 
 
 """ Server , method that launch the Flask's integrated developement web server """
 
-""" the idiom             if __name = '__name__' 
+""" the idiom             if __name = '__main__' 
 is used to ensure that the script is launch only when executed directly"""
 # debug=True can be changed when the development is over so debug = False then 
-if __name__ == '__name__':
-    app.run(debug=True)
+if __name__ == '__main__':
+ app.run(debug=True)
 
 
 
