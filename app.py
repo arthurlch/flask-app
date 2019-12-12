f9a38e4b967490b01b8a232dde504810db014361
ArthurDav <lachat.arthur74@gmail.com>
2019-12-12 16:41:03 +0900

    app.py updated, html layout created, readme updated

--- app.py
+++ app.py
@@ -5,18 +5,21 @@ Application using the python framework ' Flask '
 Flask official website: https://palletsprojects.com/p/flask/
 Flask documentation: http://flask.palletsprojects.com/en/1.1.x/
 
-Features: 
+This application is a sample to my study about Back-end and python framework and will serve the basic feautures of a modern app.
+
+Feel free to use this projects for whatever purposes. 
+
+
+Features  of the app: 
 Login/Logout
 Post message
-Blog type
+Blog type / User input 
 User management
 User Registration
 
 
-
 """
 ###
-
 
 from flask import Flask, url_for, render_template
 # import flask framework 
@@ -24,64 +27,49 @@ from flask import Flask, url_for, render_template
 # define name app app = __name__
 app = Flask(__name__)
 
-# .route decorator 
-# define the route of the pageORurl (routing)
-# .route decorator bind function to an url
-# root https://myapp.com/  root 
-# Other routes will be be for example https://myapp.com/user-profil
-@app.route('/')
-# 1 function with two routes / & /home
-@app.route('/home')
-# Function which do the rendering 
-# return a template 
-# templates are html templates and are located in flask_app/templates/ 
-def home():
-	return render_template('home.html')
 
-# About page
-# def(about) route /about
+
+
+# .route decorator for home define home page + render template of home 
+@app.route("/")
+@app.route("/home") 
+def home():
+	return render_template('home.html', posts=posts)
+# dummy data 
+# the dummy data will serve to show how to transfer data from app.py ( flask to templates made in html)
+posts = [
+{
+	'author':'Author',
+	'title':'post title',
+	'content':'First post of content',
+	'date_posted':'April 20, 2018'
+}
+]
+
+# def about page route + render 
 @app.route('/about')
 def about():
-	return ''
+	return render_template('about.html', about_profil=about_profil, title='About')
+
+about_profil = [{
+	'user':'name'
+}]
 
 
-# user section 
-@app.route('/user/<username>')
-# def username 	profil funct
-def profile(username):
-	# show user profil 
-	return "{}'s profile".format(username)
-
-# Login d
-# def login route & associated func 
-# login  hashing & sec 
+@app.route('/register')
+def register_user():
+	return render_template('register_user.html')
 
 @app.route('/login')
-def login_func():
-	return ' Attempting to login?'
+def login():	
+	return render_template('login')
 
-
-"""/
-/login
-/login?next=/
-/user/john%Doe
-"""
-
-#login
-@app.route('/login')
-def login():
-	return 'login'
-
-
-
-# users post sectin 
-# converter int: post_id = integers 
-# converters "string:,int:,float:,path:"
-@app.route('/post/<int:post_id>')
-# def user post_id func 
-def show_post(post_id):
-	return 'Post %d' % post_id 
-
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html', name=name)
+name = [{
+	'name':'name'
+}]
 
 # python idiom __name_ = '__main__' 
 # open file using the terminal ' python3 app.py'
